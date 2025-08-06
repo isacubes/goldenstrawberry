4d963ba400731b789abad557e9f091319d5dc979
i <isacubes247@gmail.com>
2025-08-06 10:40:08 -0600

    initialize repo

---
+++ localization/default.lua
@@ -0,0 +1,18 @@
+return {
+    descriptions = {
+        alt_texture = {
+            alt_tex_goldenstrawberry_goldenstrawberry = {
+                j_golden = { name = 'Golden Strawberry' }
+            }
+        },
+        texture_packs = {
+            texpack_goldenstrawberry_goldenstrawberry = {
+                name = 'Golden Strawberry',
+                text = {
+                    'Retextures {C:money}Golden Joker{} to',
+                    'the {C:money}Golden Strawberry{} from {C:spectral}Celeste{}'
+                }
+            }
+        }
+    }
+}
---
+++ main.lua
@@ -0,0 +1,26 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: GoldenStrawberry
+--- MOD_ID: goldenstrawberry
+--- PREFIX: goldenstrawberry
+--- MOD_AUTHOR: [isacubes]
+--- MOD_DESCRIPTION: Textures Golden Joker as a Golden Strawberry from Celeste
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+AltTexture{
+    key = 'goldenstrawberry',
+    set = 'Joker',
+    path = 'GoldenStrawberry.png',
+    keys = {
+        'j_golden'
+    },
+    localization = true
+}
+
+
+TexturePack{
+    key = 'goldenstrawberry',
+    textures = {
+        'goldenstrawberry_goldenstrawberry'
+    }
+}
